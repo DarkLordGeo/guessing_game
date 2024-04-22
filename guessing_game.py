559c7ba5cb0b8e90dbd42cdcad659b28ab3d3d6c
DarkLordGeo <148902362+DarkLordGeo@users.noreply.github.com>
2024-04-22 16:49:46 +0400

    Update guessing_game.py
    added comments

--- guessing_game.py
+++ guessing_game.py
@@ -1,32 +1,41 @@
-# damn!
+# everything written below is written by begineer , feel free to share advices , thanks.
+
+
+# outputing list of choices
 print("1","Animals")
 print("2","Food")
 print("3","Colors")
-
+# options that user should choose
 options = {
     "1" : "Animals",
     "2" : "Food",
     "3" : "Colors",
 }
-
+# answers
 user_answer = "tiger"
 user_answer2 ="burger"
 user_answer3 ="white"
-
+# hints
 hint_1 = "It's a large cat with distinctive orange fur and black stripes."
 hint_2 = "It's a sandwich consisting of a grilled or fried meat patty, typically beef, served in a sliced bun with various toppings and condiments."
 hint_3 = "Mix of all the colors "
 
+# using while loop
 
 while True:
     user_choice = input("Choose category you want to play: ")
 
-    
+    # if user choice equals to 1 meaning that user is choosing option 1
+    # then if they match we get ouputs
 
     if user_choice == "1":
         print("Okay, guess the animal")
         print("Type 'h' for hint")
 
+# here i defined amount of max attempts 
+# so while attempts are less then max_attempts i.e 3 then it will continue to print given input
+        
+        # main logic for the whole operation
         max_attempts = 3
         attempts = 0
         while attempts < max_attempts:
@@ -36,12 +45,20 @@ while True:
             elif user_input.lower() == "tiger":
                 print("YOU WIN!")
                 break
+            # if we get correct answer loop breaks and
             else:
                 attempts += 1
                 if attempts < max_attempts:
                     print("Sorry, try again please :( ...")
+                    # this will continue untill it will be less or equal
         else:
             print("Sorry, you've reached the maximum number of attempts:  ")
+            # here goes else if anything goes wrong
+
+    # if user choice equals to 2 meaning that user is choosing option 2
+    # then if they match we get ouputs
+
+    # similar logic to the food
     elif user_choice == "2":
         print("Okay, guess the food")
         print("Type 'h' for hint")
@@ -59,6 +76,9 @@ while True:
                 attempts += 1
                 if attempts < max_attempts:
                     print("Sorry, try again please :( ...")
+   
+    # similar logic to the color aswell
+    
     elif user_choice == "3":
         print("Okay, guess the color")
         print("Type 'h' for hint")
@@ -77,7 +97,7 @@ while True:
                 if attempts < max_attempts:
                     print("Sorry, try again please :( ...")
            
-
+    # the 'q' will stop the loop and break it 
     if user_choice == 'q':
         print("Quitting game..")
         break      
